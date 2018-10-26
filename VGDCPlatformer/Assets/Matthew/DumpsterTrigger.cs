6ef96b09a33e61f8121143605c19df19f48dbe19
Matthew Fernandez <mattpixel7@gmail.com>
2018-10-26 03:30:53 -0700

    Finished full dive animation; improved docs on DumpsterTrigger.cs; renamed DumpsterTest to Matthew

--- VGDCPlatformer/Assets/Matthew/DumpsterTrigger.cs
+++ VGDCPlatformer/Assets/Matthew/DumpsterTrigger.cs
@@ -3,7 +3,13 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class DumpsterTrigger : MonoBehaviour {
-    public float diveAnimationSeconds;
+    [Header("Powerup Settings")]
+    public string PowerUp;
+
+    [Header("Dive Animation")]
+    public float diveIdleSeconds;
+    public float diveUpForce = 0f;
+    public float diveUpSeconds = 1;
 
     // MAKE SURE PLAYER OBJECT HAS "Player" TAG!
     private GameObject playerObject;
@@ -24,7 +30,6 @@ public class DumpsterTrigger : MonoBehaviour {
     void Start () {
         playerObject = GameObject.FindGameObjectWithTag("Player");
         GetPlayerComponents(playerObject);
-
         dumpsterCollider = GetComponent<BoxCollider2D>(); // For DoDiveAnimation()
 	}
 
@@ -32,24 +37,24 @@ public class DumpsterTrigger : MonoBehaviour {
     {
         if (playerOnDumpster && Input.GetButtonDown("Vertical") && Input.GetAxisRaw("Vertical") < 0) // Button is currently "s"
         { 
-            Debug.Log("Player interacted with dumpster.");
+            Debug.Log("Player is diving.");
             OnDumpsterInteract();
         } 
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.tag == "Player") // Need to make sure only the player can trigger the animation
+        if (collision.gameObject == playerObject) // Need to make sure only the player can trigger the animation
         { 
-            Debug.Log("Player jumped onto dumpster!");
+            Debug.Log("Player on dumpster trigger");
             playerOnDumpster = true;
         }
     }
     private void OnTriggerExit2D(Collider2D collision)
     {
-        if (collision.tag == "Player")
+        if (collision.gameObject == playerObject)
         {
-            Debug.Log("Player left dumpster");
+            Debug.Log("Player left dumpster trigger");
             playerOnDumpster = false;
         }
     }
@@ -73,27 +78,26 @@ public class DumpsterTrigger : MonoBehaviour {
         playerSprite.color = new Color(0, 0, 0, 0);
         Debug.Log("Player is now frozen");
 
-        yield return new WaitForSeconds(diveAnimationSeconds);
+        yield return new WaitForSeconds(diveIdleSeconds);
 
         playerRigidBody.constraints = RigidbodyConstraints2D.FreezeRotation;
         playerSprite.color = new Color(1, 1, 1, 1);
         Debug.Log("Player can move again.");
     }
 
-
-
-
     private IEnumerator DoDiveAnimation() // Polishing this up. Not sure if it'll be implemented in the future.
     {
         playerRigidBody.constraints = RigidbodyConstraints2D.FreezePositionX | RigidbodyConstraints2D.FreezeRotation;
-        Vector3 oldPlayerLocation = playerObject.transform.position;
         dumpsterCollider.enabled = false;
-        Debug.Log("Player is now in dumpster");
 
-        yield return new WaitForSeconds(diveAnimationSeconds);
 
+        Debug.Log("Player is diving");
+
+        yield return new WaitForSeconds(diveIdleSeconds);
+
+        playerRigidBody.AddForce(new Vector3(0f, diveUpForce, 0f));
+        yield return new WaitForSeconds(diveUpSeconds);
         dumpsterCollider.enabled = true;
-        playerObject.transform.position = oldPlayerLocation;
         playerRigidBody.constraints = RigidbodyConstraints2D.FreezeRotation;
         Debug.Log("Player can move again.");
     }
