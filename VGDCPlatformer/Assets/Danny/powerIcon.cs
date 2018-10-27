f316714b2f7541933400b23727e89e8db131b999
Danny QI <drqi@uci.edu>
2018-10-27 13:38:56 -0700

    Got powerups working
    only speed and jump for now, will add attack later (everything dies in one hit anyways)
    
    displays the icon above player head 
    
    lasts for 10 seconds (Can shorten if needed)

---
+++ VGDCPlatformer/Assets/Danny/powerIcon.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class powerIcon : MonoBehaviour {
+
+    GameObject power;
+    SpriteRenderer powerSprite;
+    SpriteRenderer playerSprite;
+
+	// Use this for initialization
+	void Start () {
+        power = GameObject.FindGameObjectWithTag("powerup");
+        powerSprite = power.GetComponent<SpriteRenderer>();
+        playerSprite = GameObject.FindGameObjectWithTag("Player").GetComponent<SpriteRenderer>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        alignCenter();
+	}
+
+    void alignCenter()
+    {
+        Vector3 pVect = new Vector3(playerSprite.bounds.center.x, playerSprite.bounds.center.y + (playerSprite.bounds.size.y / (7/4)));
+        powerSprite.transform.position = pVect;
+    }
+}
