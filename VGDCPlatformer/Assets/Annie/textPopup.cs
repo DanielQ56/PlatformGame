f530a2c7ef5051bbb055185239e9a1c3e8e520a9
Annie Xu <axu.1492@gmail.com>
2018-11-09 15:18:40 -0800

    small change

--- VGDCPlatformer/Assets/Annie/textPopup.cs
+++ VGDCPlatformer/Assets/Annie/textPopup.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class textPopup : MonoBehaviour {
 
 	public string showThisText;
-
+	private bool showText;
 	// Use this for initialization
 	void Start () {
 		
@@ -13,6 +13,21 @@ public class textPopup : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
+		
+	}
+	
+	void OnTriggerEnter2D(Collider2D collid) {
+		if (collid.tag == "Player") {
+			showText = true;
+		}
+		
+	}
+	
+	void OnGUI() {
+		if (showText == true) {
+			
+			
+		}
 		
 	}
 }
