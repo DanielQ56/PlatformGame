67e899b6880edf6795e626109741b4a8e356fd24
Annie Xu <axu.1492@gmail.com>
2018-11-20 12:33:18 -0800

    animation flips correctly

--- VGDCPlatformer/Assets/Annie/RaccoonAnimationController.cs
+++ VGDCPlatformer/Assets/Annie/RaccoonAnimationController.cs
@@ -4,55 +4,50 @@ using System.Collections;
 
 public class RaccoonAnimationController : MonoBehaviour {
 
-	private bool facingRight = true;
-
-	// This will be a reference to the RigidBody2D 
-	// component in the Player object
 	private Rigidbody2D rb;
-
+	private CharacterController2D charControl;
+	private SpriteRenderer spriteRend;
 	// This is a reference to the Animator component
 	private Animator anim;
 
-	private float playerSpeed = 0.1f;
+	// Reference to PlayerMove
+	private PlayerMove pMove;
+	
+	private float playerSpeed = 0f;
+	private bool animFacingRight = true;
 
+	
 	// We initialize our two references in the Start method
 	void Start() {
 		rb = GetComponent<Rigidbody2D>();
+		charControl = GetComponent<CharacterController2D>();
+		spriteRend = GetComponent<SpriteRenderer>();
+		pMove = GetComponent<PlayerMove>();
 		anim = GetComponent<Animator>();
+		//animFacingRight = charControl.m_FacingRight;
 	}
 
+
+	
 	// We use FixedUpdate to do all the animation work
 	void FixedUpdate() {
 		//GET PLAYER SPEED 
-		
+		playerSpeed = Input.GetAxisRaw("Horizontal");
 		//GET PLAYER FACING RIGHT 
-		
-		// Get the extent to which the player is currently pressing left or right
-		//float h = Input.GetAxis("Horizontal")
-		// Pass in the current velocity of the RigidBody2D
+		//playerFacingRight = charControl.m_FacingRight;
 		// The speed parameter of the Animator now knows
 		// how fast the player is moving and responds accordingly
-		anim.SetFloat("Speed", 0.1f);
+		anim.SetFloat("Speed", Mathf.Abs(playerSpeed));
 
 
-		// Check which way the player is facing 
-		// and call reverseImage if neccessary
-		if (playerSpeed < 0 && facingRight)
-			reverseImage();
-		else if (playerSpeed > 0 && !facingRight)
-			reverseImage();
 
 	}
 
 	void reverseImage()
 	{
-		// Switch the value of the Boolean
-		facingRight = !facingRight;
-
+		animFacingRight = !animFacingRight;
 		// Get and store the local scale of the RigidBody2D
 		Vector2 theScale = rb.transform.localScale;
-
-		// Flip it around the other way
 		theScale.x *= -1;
 		rb.transform.localScale = theScale;
 	}
