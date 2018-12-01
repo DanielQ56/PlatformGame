becbb24c7d33c3a482c8e9cf3979045ffeb439e5
Annie Xu <axu.1492@gmail.com>
2018-11-30 23:31:03 -0800

    working tutorial scene pending art

---
+++ VGDCPlatformer/Assets/Annie/IntroTutorial/swapObjects.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class swapObjects: MonoBehaviour {
+
+	public GameObject oldObject;
+	public GameObject newObject;
+	public bool useDumpCond = false;
+	private bool showText;
+	private bool playerOnDumpster = false;
+	
+	// Use this for initialization
+	void Start () {
+		if(newObject != null) {
+			newObject.SetActive(false);
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(useDumpCond && playerOnDumpster && Input.GetButtonDown("Vertical")) {
+			oldObject.SetActive(false);
+			newObject.SetActive(true);
+		}
+	}
+	
+	void OnTriggerEnter2D(Collider2D collid) {
+		if (collid.gameObject.tag == "Player") {
+			playerOnDumpster = true;
+		}
+	}
+}
---
+++ VGDCPlatformer/Assets/Annie/IntroTutorial/tutorialGroundRespawn.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class tutorialGroundRespawn : MonoBehaviour {
+
+	private Vector3 originalPlayerPosition;
+	private GameObject player;
+
+	// Use this for initialization
+	void Start () {
+		player = GameObject.FindWithTag("Player");
+		originalPlayerPosition = player.transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+	
+	public void OnCollisionEnter2D(Collision2D collision)
+    {
+        //set the player as a child to the platform gaemObject
+        //this allows movement of the player with moving platforms
+        if (collision.gameObject.CompareTag("Player"))
+        { 
+			player.transform.position = originalPlayerPosition;
+        }
+    }
+	
+}
