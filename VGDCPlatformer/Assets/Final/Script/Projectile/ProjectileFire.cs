425c8ba8a91156cb2405e5eb35a42c85b7c2e249
Annie Xu <axu.1492@gmail.com>
2018-11-30 18:13:46 -0800

    changed player collider and launcher/projectile sprites

--- VGDCPlatformer/Assets/Final/Script/Projectile/ProjectileFire.cs
+++ VGDCPlatformer/Assets/Final/Script/Projectile/ProjectileFire.cs
@@ -6,10 +6,15 @@ public class ProjectileFire : MonoBehaviour {
 
 	public GameObject projectilePrefab;
 	public GameObject player;
-    private PlayerAmmo ammoControl;
+	private PlayerAmmo ammoControl;
+	public float launchSpeed = 3f;
 	private bool parentFacingRight;
 	public int ammoCount;
+	public float timeBetweenProj = 0.3333f;
+	private float timestamp;
 
+	public Sprite[] projectileSprites;
+	
 	// Use this for initialization
 	void Start () {
 		ammoControl = player.GetComponent<PlayerAmmo>();
@@ -18,20 +23,16 @@ public class ProjectileFire : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-
 		ammoCount = ammoControl.getCurrentAmmo();
-
-        //if (Input.GetButtonDown("Fire1")) {
-        //	FireProj();
-        //}
-        if (Input.GetMouseButtonDown(0) && ammoCount > 0) {
+		//if (Input.GetButtonDown("Fire1")) {
+		//	FireProj();
+		//}
+		if (Input.GetMouseButtonDown(0) && ammoCount > 0 && Time.time >= timestamp) {
 			Vector2 shootDirection;
 			shootDirection=Camera.main.ScreenToWorldPoint(Input.mousePosition)-transform.position;
 			FireDirectedProj(shootDirection);
 			ammoControl.decreaseAmmo(1);
-			if (ammoCount == 0) {
-				Debug.Log("No more ammo");
-			}
+			timestamp = Time.time + timeBetweenProj;
 		}
 		
 	}
@@ -41,13 +42,15 @@ public class ProjectileFire : MonoBehaviour {
 		parentFacingRight = player.GetComponent<CharacterController2D>().m_FacingRight;
 		GameObject Clone;
 		Clone = (Instantiate(projectilePrefab, transform.position, transform.rotation));
-        Clone.GetComponent<Projectile>().facingRight = parentFacingRight;	
+		Clone.GetComponent<projectileScript>().facingRight = parentFacingRight;	
 	}
 	
-	public void FireDirectedProj(Vector3 direct) {
+	public void FireDirectedProj(Vector2 direct) {
+		int i = Random.Range(0, projectileSprites.Length);
 		GameObject Clone;
 		Clone = Instantiate(projectilePrefab, transform.position, transform.rotation);
-		Clone.GetComponent<Rigidbody2D>().velocity = new Vector2 (direct.x, direct.y);
+		Clone.GetComponent<SpriteRenderer>().sprite = projectileSprites[i];
+		Clone.GetComponent<Rigidbody2D>().velocity = new Vector2(direct.x, direct.y).normalized * launchSpeed;
 		
 		
 	}
