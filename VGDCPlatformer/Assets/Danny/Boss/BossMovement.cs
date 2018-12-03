b1df5363382b55f1d5a64cef504fbb4d3dc78bbc
Annie Xu <axu.1492@gmail.com>
2018-12-03 01:21:08 -0800

    commented out sprite flipping in bossmove script

--- VGDCPlatformer/Assets/Danny/Boss/BossMovement.cs
+++ VGDCPlatformer/Assets/Danny/Boss/BossMovement.cs
@@ -30,7 +30,7 @@ public class BossMovement : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-       flipSpriteDirection();
+       //flipSpriteDirection();
 	}
 
     void FixedUpdate()
