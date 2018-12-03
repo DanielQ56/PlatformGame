ee7efefb9fbe96d5aceb1eec4386e394d8b84d04
Annie Xu <axu.1492@gmail.com>
2018-12-03 01:16:13 -0800

    added enemy animation

---
+++ VGDCPlatformer/Assets/Final/Script/BossAnimationController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BossAnimationController : MonoBehaviour {
+
+	private BossMovement bossMove;
+
+	private Animator anim;
+	private bool animFacingLeft;
+
+	// Use this for initialization
+	void Start () {
+		anim = GetComponent<Animator>();
+		bossMove = GetComponent<BossMovement>();
+		animFacingLeft = bossMove.isFacingLeft();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		animFacingLeft = bossMove.isFacingLeft();
+		anim.SetBool("facingLeft", animFacingLeft);
+	}
+}
