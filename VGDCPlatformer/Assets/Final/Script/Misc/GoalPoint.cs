867b5500a3870acab05deb6023617877d04bda38
Matthew Fernandez <mattfer98@gmail.com>
2018-12-02 16:32:13 -0800

    Fixed merge conflict

---
+++ VGDCPlatformer/Assets/Final/Script/Misc/GoalPoint.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GoalPoint : MonoBehaviour {
+	public string nextScene;
+	GameObject player;
+
+	void Start(){
+		player = GameObject.FindGameObjectWithTag("Player");
+	}
+
+	void OnTriggerEnter2D(Collider2D other) {
+		Debug.Log("Something touched the flag");
+		Debug.Log(other.gameObject.tag);
+
+		if(other.gameObject == player){
+			Debug.Log("Reached goal");
+			SceneManager.LoadScene(nextScene);
+		}
+	}
+}
